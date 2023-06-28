49abb38a87de69cb2abcbfd9db8e6f9672d2249b
yulim <yulim13sky@naver.com>
2023-06-28 16:52:08 +0900

    feat: BoardCommand 인터페이스 생성

---
+++ FinalProject/src/main/java/command/BoardCommand.java
@@ -0,0 +1,8 @@
+package command;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface BoardCommand {
+    void execute(HttpServletRequest request, HttpServletResponse response);
+}
